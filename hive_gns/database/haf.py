7dded58fb1e3aa66adeecf9764ad3ffaef6b0485
imwatsi <imwatsi@gmail.com>
2022-07-30 20:08:34 +0400

    Update haf.py

--- hive_gns/database/haf.py
+++ hive_gns/database/haf.py
@@ -1,86 +1,31 @@
+from faulthandler import is_enabled
 import json
 import os
 import re
 from threading import Thread
+import time
 from hive_gns.database.core import DbSession
+from hive_gns.database.modules import AvailableModules, Module
 
-from hive_gns.tools import INSTALL_DIR
+from hive_gns.tools import GLOBAL_START_BLOCK, INSTALL_DIR
 
 SOURCE_DIR = os.path.dirname(__file__) + "/sql"
 
 MAIN_CONTEXT = "gns"
 
 
-class Sync:
-
-    db_conn = DbSession()
-    error = False
-    
-    @classmethod
-    def _create_new_connection(cls):
-        if cls.error == False:
-            del cls.db_conn
-            cls.db_conn = DbSession()
-    
-    @classmethod
-    def _disable_sync(cls):
-        cls.db_conn.execute(
-            "UPDATE gns.global_props SET sync_enabled = false;"
-        )
-        cls.db_conn.commit()
-
-    @classmethod
-    def disable_module(cls, module):
-        cls.db_conn.execute(
-            f"UPDATE gns.module_state SET enabled = false WHERE module = '{module}';"
-        )
-        cls.db_conn.commit()
-    
-    @classmethod
-    def enable_module(cls, module):
-        cls.db_conn.execute(
-            f"UPDATE gns.module_state SET enabled = true WHERE module = '{module}';"
-        )
-        cls.db_conn.commit()
-    
-    @classmethod
-    def is_enabled(cls, module):
-        enabled = bool(
-            cls.db_conn.select_one(
-                f"SELECT enabled FROM gns.module_state WHERE module ='{module}';"
-            )
-        )
-        return enabled
-
-    @classmethod
-    def is_connection_open(cls):
-        return cls.db_conn.is_open()
-    
-    @classmethod
-    def is_sync_running(cls):
-        running = cls.db_conn.select_exists(
-            "SELECT * FROM gns.global_props WHERE check_in >= NOW() - INTERVAL '1 min'")
-        return running
-    
-    @classmethod
-    def start(cls):
-        try:
-            print("Running state_preload script...")
-            cls.db_conn.execute("CALL gns.load_state();")
-            print("HAF sync starting...")
-            cls.db_conn.execute("CALL gns.run_sync();")
-        except Exception as err:
-            print(f"HAF sync error: {err}")
-            cls.error = True
-            cls._disable_sync()
-            cls.db_conn.conn.close()
-            cls.db_conn.conn.close()
-
-
 class Haf:
 
     db = DbSession()
     module_list = []
+
+    @classmethod
+    def _get_haf_sync_head(cls):
+        sql = f"""
+            SELECT block_num, timestamp FROM hive.operations_view ORDER BY block_num DESC LIMIT 1;
+        """
+        res = cls.db.select(sql)
+        return res[0]
 
     @classmethod
     def _is_valid_module(cls, module):
@@ -100,51 +45,66 @@ class Haf:
             print(f"HAF SYNC:: created context: '{name}'")
     
     @classmethod
-    def _check_schema(cls, module, tables):
-        exists = cls.db.select(f"SELECT schema_name FROM information_schema.schemata WHERE schema_name='{module}';")
-        if exists is None:
-            cls.db.execute(tables, None)
-            cls.db.commit()
-    
-    @classmethod
     def _update_functions(cls, functions):
         cls.db.execute(functions, None)
         cls.db.commit()
     
     @classmethod
-    def _check_hooks(cls, module, defs):
-        has = cls.db.select_exists(f"SELECT module FROM gns.module_state WHERE module='{module}'")
-        # generate used op type ids array
-        _op_ids = []
-        for op in defs['ops'].keys():
-            _op_ids.append(op)
-        defs['op_ids'] = _op_ids
-        defs = json.dumps(defs)
-        if has is False:
+    def _check_hooks(cls, module, hooks):
+        enabled = hooks['enabled']
+        has_entry = cls.db.select_exists(f"SELECT module FROM gns.module_state WHERE module='{module}'")
+        if has_entry is False:
             cls.db.execute(
                 f"""
-                    INSERT INTO gns.module_state (module, hooks)
-                    VALUES ('{module}', '{defs}');
+                    INSERT INTO gns.module_state (module, enabled)
+                    VALUES ('{module}', '{enabled}');
                 """)
         else:
             cls.db.execute(
                 f"""
-                    UPDATE gns.module_state SET defs='{defs}' WHERE module='{module}';
+                    UPDATE gns.module_state SET enabled = '{enabled}'
+                    WHERE module = '{module}';
+                """)
+        del hooks['enabled']
+        # update module hooks table
+        for notif_name in hooks:
+            _notif_code = hooks[notif_name]['notif_code']
+            _funct = hooks[notif_name]['function']
+            _op_id = int(hooks[notif_name]['op_id'])
+            _filter = json.dumps(hooks[notif_name]['filter'])
+            has_hooks_entry = cls.db.select_exists(
+                f"""
+                    SELECT module FROM gns.module_hooks 
+                    WHERE module='{module}' AND notif_name = '{notif_name}'
                 """
             )
+            if has_hooks_entry is False:
+                cls.db.execute(
+                    f"""
+                        INSERT INTO gns.module_hooks (module, notif_name, notif_code, funct, op_id, notif_filter)
+                        VALUES ('{module}', '{notif_name}', '{_notif_code}', '{_funct}', '{_op_id}', '{_filter}');
+                    """
+                )
+            else:
+                cls.db.execute(
+                    f"""
+                        UPDATE gns.module_hooks 
+                        SET notif_code = '{_notif_code}',
+                            funct = '{_funct}', op_id = {_op_id}, notif_filter= '{_filter}';
+                    """
+                )
 
     @classmethod
     def _init_modules(cls):
         working_dir = f'{INSTALL_DIR}/modules'
         cls.module_list = [f.name for f in os.scandir(working_dir) if cls._is_valid_module(f.name)]
         for module in cls.module_list:
-            defs = json.loads(open(f'{working_dir}/{module}/defs.json', 'r', encoding='UTF-8').read())
+            hooks = json.loads(open(f'{working_dir}/{module}/hooks.json', 'r', encoding='UTF-8').read())
             functions = open(f'{working_dir}/{module}/functions.sql', 'r', encoding='UTF-8').read()
-            tables = open(f'{working_dir}/{module}/tables.sql', 'r', encoding='UTF-8').read()
-            cls._check_context(module, defs['props']['start_block'])
-            cls._check_schema(module, tables)
-            cls._check_hooks(module, defs)
+            cls._check_context(module)
+            cls._check_hooks(module, hooks)
             cls._update_functions(functions)
+            AvailableModules.add_module(module, Module(module, hooks))
 
     @classmethod
     def _init_gns(cls):
@@ -152,17 +112,29 @@ class Haf:
         for _file in ['tables.sql', 'functions.sql', 'sync.sql', 'state_preload.sql', 'filters.sql']:
             _sql = open(f'{SOURCE_DIR}/{_file}', 'r', encoding='UTF-8').read()
             cls.db.execute(_sql)
-        cls.db.execute(
-            """
-                INSERT INTO gns.global_props (head_block_num)
-                SELECT '0'
-                WHERE NOT EXISTS (SELECT * FROM gns.global_props);
-            """, None
-        )
         cls.db.commit()
+        has_globs = cls.db.select("SELECT * FROM gns.global_props;")
+        if not has_globs:
+            cls.db.execute("INSERT INTO gns.global_props (check_in) VALUES (NULL);")
+            cls.db.commit()
+    
+    @classmethod
+    def _init_pruner(cls):
+        while True:
+            ready = cls.db.select_one("SELECT state_preloaded FROM gns.global_props;")
+            if ready is True:
+                break
+            time.sleep(60)
+        while True:
+            cls.db.execute("CALL gns.run_pruner();")
+            time.sleep(30)
 
     @classmethod
     def init(cls):
         cls._init_gns()
         cls._init_modules()
-        Thread(target=Sync.start).start()
+        print("Running state_preload script...")
+        end_block = cls._get_haf_sync_head()[0]
+        #cls.db.execute(f"CALL gns.load_state({GLOBAL_START_BLOCK}, {end_block});")
+        Thread(target=AvailableModules.module_watch).start()
+        #Thread(target=cls._init_pruner).start()
