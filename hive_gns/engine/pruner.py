5c21163540dcf810d54292fb7a7d6beb38b69186
imwatsi <imwatsi@freebeings.io>
2022-11-05 11:21:48 +0400

    enable pruner

--- hive_gns/engine/pruner.py
+++ hive_gns/engine/pruner.py
@@ -1,19 +1,22 @@
 import time
+from threading import Thread
 
+from hive_gns.config import Config
 from hive_gns.database.access import db
+
+config = Config.config
 
 class Pruner:
 
     @classmethod
-    def _delete_old_ops(cls):
-        sql = """
-            DELETE FROM gns.ops
-            WHERE created <= NOW() - INTERVAL '30 DAYS';
-        """.replace("gns.", f"{config['schema']}.")
-        return db.delete(sql)
+    def _run(cls):
+        while True:
+            sql_gns = "SELECT gns.prune_gns();".replace("gns.", f"{config['schema']}.")
+            db.execute(sql_gns)
+            #sql_haf = "SELECT gns.prune_haf();".replace("gns.", f"{config['schema']}.")
+            #db.execute(sql_haf)
+            time.sleep(300)
 
     @classmethod
     def run_pruner(cls):
-        while True:
-            cls._delete_old_ops()
-            time.sleep(300)
+        Thread(target=cls._run).start()
