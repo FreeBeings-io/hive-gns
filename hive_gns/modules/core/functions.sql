bb95222b06cdad2198cb3a24291bf63390ab62ff
imwatsi <imwatsi@freebeings.io>
2023-02-02 18:21:32 +0200

    fix comments and delegs

--- hive_gns/modules/core/functions.sql
+++ hive_gns/modules/core/functions.sql
@@ -178,10 +178,14 @@ CREATE OR REPLACE FUNCTION gns.core_comment( _gns_op_id BIGINT, _trx_id BYTEA, _
             _permlink := _body->'value'->>'permlink';
 
             IF length(_parent_author) > 0 AND length(_parent_permlink) > 0 THEN
-                -- check acount
+                -- check parent_author acount
                 INSERT INTO gns.accounts (account)
                 SELECT _parent_author
                 WHERE NOT EXISTS (SELECT * FROM gns.accounts WHERE account = _parent_author);
+                -- check author acount
+                INSERT INTO gns.accounts (account)
+                SELECT _author
+                WHERE NOT EXISTS (SELECT * FROM gns.accounts WHERE account = _author);
 
                 -- check if subscribed
                 _sub := gns.check_user_filter(_parent_author, 'core', _notif_code);
@@ -195,6 +199,57 @@ CREATE OR REPLACE FUNCTION gns.core_comment( _gns_op_id BIGINT, _trx_id BYTEA, _
                     INSERT INTO gns.account_notifs (gns_op_id, trx_id, account, module_name, notif_code, created, remark, payload, verified, link)
                     VALUES (_gns_op_id, _trx_id, _author, 'core', _notif_code, _created, _remark, _body, true, _link);
                 END IF;
+            END IF;
+
+            -- RAISE NOTICE 'value: % \n', _value;
+
+        EXCEPTION WHEN OTHERS THEN
+                RAISE NOTICE E'Got exception:
+                SQLSTATE: % 
+                SQLERRM: %
+                DATA: %', SQLSTATE, SQLERRM, _body;
+        END;
+        $function$;
+
+CREATE OR REPLACE FUNCTION gns.core_deleg( _gns_op_id BIGINT, _trx_id BYTEA, _created TIMESTAMP, _body JSON, _notif_code VARCHAR(3) )
+    RETURNS void
+    LANGUAGE plpgsql
+    VOLATILE AS $function$
+        DECLARE
+            _delegator VARCHAR(16);
+            _delegatee VARCHAR(16);
+            _amount BIGINT;
+            _vests NUMERIC;
+            --_json_metadata
+            _remark VARCHAR(500);
+            _sub BOOLEAN;
+            _link VARCHAR(500);
+
+        BEGIN
+            _delegator := _body->'value'->>'delegator';
+            _delegatee := _body->'value'->>'delegatee';
+            _amount := _body->'value'->'vesting_shares'->>'amount';
+            _vests := round((_amount::numeric)/1000000, 6);
+
+            INSERT INTO gns.accounts (account)
+            SELECT _delegatee
+            WHERE NOT EXISTS (SELECT * FROM gns.accounts WHERE account = _delegatee);
+
+            -- check if subscribed
+            _sub := gns.check_user_filter(_delegatee, 'core', _notif_code);
+
+            IF _sub = true THEN
+
+                IF _amount > 0 THEN
+                    _remark := FORMAT('%s is now delegating %s VESTS to you', _delegator, _vests);
+                ELSE
+                    _remark := FORMAT('%s removed their vesting delegation', _delegator);
+                END IF;
+                _link := FORMAT('https://hive.blog/@%s', _delegator);
+
+                -- make notification entry
+                INSERT INTO gns.account_notifs (gns_op_id, trx_id, account, module_name, notif_code, created, remark, payload, verified, link)
+                VALUES (_gns_op_id, _trx_id, _delegatee, 'core', _notif_code, _created, _remark, _body, true, _link);
             END IF;
 
             -- RAISE NOTICE 'value: % \n', _value;
